8b999b16b615d4922ee41a7016c6bca5f48f15e4
TheBlueGamer-Github <alexjokr@outlook.com>
2025-11-15 20:13:24 -0600

    Add Sonic 1 Mapping files

---
+++ _maps/Crabmeat.asm
@@ -0,0 +1,79 @@
+Map_77f6: mappingsTable
+	mappingsTableEntry.w	Map_77f6_0
+	mappingsTableEntry.w	Map_77f6_1
+	mappingsTableEntry.w	Map_77f6_2
+	mappingsTableEntry.w	Map_77f6_3
+	mappingsTableEntry.w	Map_77f6_4
+	mappingsTableEntry.w	Map_77f6_5
+	mappingsTableEntry.w	Map_77f6_6
+	mappingsTableEntry.w	Map_77f6_7
+	mappingsTableEntry.w	Map_77f6_8
+	mappingsTableEntry.w	Map_77f6_9
+
+Map_77f6_0:	spriteHeader
+ spritePiece -$18, -$10, 3, 2, 0, 0, 0, 0, 0
+ spritePiece 0, -$10, 3, 2, 0, 1, 0, 0, 0
+ spritePiece -$10, 0, 2, 2, 6, 0, 0, 0, 0
+ spritePiece 0, 0, 2, 2, 6, 1, 0, 0, 0
+Map_77f6_0_End
+
+Map_77f6_1:	spriteHeader
+ spritePiece -$18, -$10, 3, 2, $A, 0, 0, 0, 0
+ spritePiece 0, -$10, 3, 2, $10, 0, 0, 0, 0
+ spritePiece -$10, 0, 2, 2, $16, 0, 0, 0, 0
+ spritePiece 0, 0, 3, 2, $1A, 0, 0, 0, 0
+Map_77f6_1_End
+
+Map_77f6_2:	spriteHeader
+ spritePiece -$18, -$14, 3, 2, 0, 0, 0, 0, 0
+ spritePiece 0, -$14, 3, 2, 0, 1, 0, 0, 0
+ spritePiece 0, -4, 2, 2, 6, 1, 0, 0, 0
+ spritePiece -$10, -4, 2, 3, $20, 0, 0, 0, 0
+Map_77f6_2_End
+
+Map_77f6_3:	spriteHeader
+ spritePiece -$18, -$14, 3, 2, $A, 0, 0, 0, 0
+ spritePiece 0, -$14, 3, 2, $10, 0, 0, 0, 0
+ spritePiece 0, -4, 3, 2, $26, 0, 0, 0, 0
+ spritePiece -$10, -4, 2, 3, $2C, 0, 0, 0, 0
+Map_77f6_3_End
+
+Map_77f6_4:	spriteHeader
+ spritePiece -$10, -$10, 2, 1, $32, 0, 0, 0, 0
+ spritePiece 0, -$10, 2, 1, $32, 1, 0, 0, 0
+ spritePiece -$18, -8, 3, 2, $34, 0, 0, 0, 0
+ spritePiece 0, -8, 3, 2, $34, 1, 0, 0, 0
+ spritePiece -$10, 8, 2, 1, $3A, 0, 0, 0, 0
+ spritePiece 0, 8, 2, 1, $3A, 1, 0, 0, 0
+Map_77f6_4_End
+
+Map_77f6_5:	spriteHeader
+ spritePiece -8, -8, 2, 2, $3C, 0, 0, 0, 0
+Map_77f6_5_End
+
+Map_77f6_6:	spriteHeader
+ spritePiece -8, -8, 2, 2, $40, 0, 0, 0, 0
+Map_77f6_6_End
+
+Map_77f6_7:	spriteHeader
+ spritePiece 0, -$10, 3, 2, $A, 1, 0, 0, 0
+ spritePiece -$18, -$10, 3, 2, $10, 1, 0, 0, 0
+ spritePiece 0, 0, 2, 2, $16, 1, 0, 0, 0
+ spritePiece -$18, 0, 3, 2, $1A, 1, 0, 0, 0
+Map_77f6_7_End
+
+Map_77f6_8:	spriteHeader
+ spritePiece 0, -$14, 3, 2, 0, 1, 0, 0, 0
+ spritePiece -$18, -$14, 3, 2, 0, 0, 0, 0, 0
+ spritePiece -$10, -4, 2, 2, 6, 0, 0, 0, 0
+ spritePiece 0, -4, 2, 3, $20, 1, 0, 0, 0
+Map_77f6_8_End
+
+Map_77f6_9:	spriteHeader
+ spritePiece 0, -$14, 3, 2, $A, 1, 0, 0, 0
+ spritePiece -$18, -$14, 3, 2, $10, 1, 0, 0, 0
+ spritePiece -$18, -4, 3, 2, $26, 1, 0, 0, 0
+ spritePiece 0, -4, 2, 3, $2C, 1, 0, 0, 0
+Map_77f6_9_End
+
+	even
---
+++ _maps/Fireballs.asm
@@ -0,0 +1,53 @@
+Map_73de: mappingsTable
+	mappingsTableEntry.w	Map_73de_0
+	mappingsTableEntry.w	Map_73de_1
+	mappingsTableEntry.w	Map_73de_2
+	mappingsTableEntry.w	Map_73de_3
+	mappingsTableEntry.w	Map_73de_4
+	mappingsTableEntry.w	Map_73de_5
+	mappingsTableEntry.w	Map_73de_6
+	mappingsTableEntry.w	Map_73de_7
+	mappingsTableEntry.w	Map_73de_8
+	mappingsTableEntry.w	Map_73de_9
+
+Map_73de_0:	spriteHeader
+ spritePiece -8, -$18, 2, 4, 0, 0, 0, 0, 0
+Map_73de_0_End
+
+Map_73de_1:	spriteHeader
+ spritePiece -8, -$18, 2, 4, 8, 0, 0, 0, 0
+Map_73de_1_End
+
+Map_73de_2:	spriteHeader
+ spritePiece -8, -$10, 2, 3, $10, 0, 0, 0, 0
+Map_73de_2_End
+
+Map_73de_3:	spriteHeader
+ spritePiece -$18, -8, 4, 2, $16, 0, 0, 0, 0
+Map_73de_3_End
+
+Map_73de_4:	spriteHeader
+ spritePiece -$18, -8, 4, 2, $1E, 0, 0, 0, 0
+Map_73de_4_End
+
+Map_73de_5:	spriteHeader
+ spritePiece -$10, -8, 3, 2, $26, 0, 0, 0, 0
+Map_73de_5_End
+
+Map_73de_6:	spriteHeader
+ spritePiece -8, -$18, 2, 4, 0, 1, 0, 0, 0
+Map_73de_6_End
+
+Map_73de_7:	spriteHeader
+ spritePiece -8, -$18, 2, 4, 8, 1, 0, 0, 0
+Map_73de_7_End
+
+Map_73de_8:	spriteHeader
+ spritePiece -$18, -8, 4, 2, $16, 0, 1, 0, 0
+Map_73de_8_End
+
+Map_73de_9:	spriteHeader
+ spritePiece -$18, -8, 4, 2, $1E, 0, 1, 0, 0
+Map_73de_9_End
+
+	even
