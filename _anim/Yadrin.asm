a7e651685e2da93c20c7b0f5e08a187863f327b6
TheBlueGamer-Github <alexjokr@outlook.com>
2025-11-22 16:34:00 -0600

    Fix error with Yadrin Animations

--- _anim/Yadrin.asm
+++ _anim/Yadrin.asm
@@ -1,10 +1,10 @@
 ; ---------------------------------------------------------------------------
 ; Animation script - Yadrin enemy
 ; ---------------------------------------------------------------------------
-Ani_Yad:	dc.w .stand-Ani_Yad
-		dc.w .walk-Ani_Yad
+Ani_Yad:	dc.w StandYad-Ani_Yad
+		dc.w WalkYad-Ani_Yad
 
-.stand:		dc.b 7,	0, afEnd
+StandYad:		dc.b 7,	0, afEnd
 		even
-.walk:		dc.b 7,	0, 3, 1, 4, 0, 3, 2, 5,	afEnd
+WalkYad:		dc.b 7,	0, 3, 1, 4, 0, 3, 2, 5,	afEnd
 		even
