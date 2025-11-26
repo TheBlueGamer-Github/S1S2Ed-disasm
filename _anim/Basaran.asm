ae00c907b7babb7fef58a2825eabbf03f63a515b
TheBlueGamer-Github <alexjokr@outlook.com>
2025-11-26 13:40:23 -0600

    Fix build errors with animations

--- _anim/Basaran.asm
+++ _anim/Basaran.asm
@@ -2,11 +2,11 @@
 ; Animation script - Basaran enemy
 ; ---------------------------------------------------------------------------
 Ani_Bas:	dc.w .still-Ani_Bas
-		dc.w .fall-Ani_Bas
-		dc.w .fly-Ani_Bas
+		dc.w fallBas-Ani_Bas
+		dc.w flyBas-Ani_Bas
 .still:		dc.b $F, 0, afEnd
 		even
-.fall:		dc.b $F, 1, afEnd
+fallBas:		dc.b $F, 1, afEnd
 		even
-.fly:		dc.b 3,	1, 2, 3, 2, afEnd
+flyBas:		dc.b 3,	1, 2, 3, 2, afEnd
 		even
