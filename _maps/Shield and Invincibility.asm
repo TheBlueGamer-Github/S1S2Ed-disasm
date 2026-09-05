166368023798b1551f40a487bbad01c7156735cb
TheBlueGamer-Github <alexjokr@outlook.com>
2026-09-05 09:40:54 -0500

    revert this stupid change

--- _maps/Shield and Invincibility.asm
+++ _maps/Shield and Invincibility.asm
@@ -1,88 +1,55 @@
-Map_1a6a: mappingsTable
-	mappingsTableEntry.w	Map_1a6a_0
-	mappingsTableEntry.w	Map_1a6a_1
-	mappingsTableEntry.w	Map_1a6a_2
-	mappingsTableEntry.w	Map_1a6a_3
+Map_Shield_internal:
+	dc.w	.shield1-Map_Shield_internal
+	dc.w	.shield2-Map_Shield_internal
+	dc.w	.shield3-Map_Shield_internal
+	dc.w	.shield4-Map_Shield_internal
+	dc.w	.stars1-Map_Shield_internal
+	dc.w	.stars2-Map_Shield_internal
+	dc.w	.stars3-Map_Shield_internal
+	dc.w	.stars4-Map_Shield_internal
 
-Map_1a6a_0:	spriteHeader
- spritePiece $18, 5, 2, 1, $11D, 0, 0, 0, 0
- spritePiece $28, 5, 1, 1, $11E, 0, 0, 0, 0
- spritePiece $30, 5, 1, 1, $11E, 0, 0, 0, 0
- spritePiece $37, 5, 1, 1, $11E, 0, 0, 0, 0
- spritePiece $3F, 5, 1, 1, $11E, 0, 0, 0, 0
- spritePiece $47, 5, 2, 1, $11E, 0, 0, 0, 0
- spritePiece $57, 5, 2, 1, $11E, 0, 0, 0, 0
- spritePiece $67, 5, 1, 1, $120, 0, 0, 0, 0
- spritePiece $24, -3, 1, 1, $F8, 0, 0, 1, 0
- spritePiece $2C, -3, 1, 1, $FE, 0, 0, 1, 0
- spritePiece $32, -3, 1, 1, $F1, 0, 0, 1, 0
- spritePiece $3A, -3, 1, 1, $100, 0, 0, 1, 0
- spritePiece $4C, -3, 1, 1, $F8, 0, 0, 1, 0
- spritePiece $54, -3, 1, 1, $FE, 0, 0, 1, 0
- spritePiece $5A, -3, 1, 1, $F1, 0, 0, 1, 0
- spritePiece $62, -3, 1, 1, $100, 0, 0, 1, 0
- spritePiece $38, $B, 1, 1, $F8, 0, 0, 0, 0
- spritePiece $40, $B, 1, 1, $FE, 0, 0, 0, 0
- spritePiece $46, $B, 1, 1, $F6, 0, 0, 0, 0
- spritePiece $4E, $B, 1, 1, $100, 0, 0, 0, 0
-Map_1a6a_0_End
+.shield1:	dc.w 0
 
-Map_1a6a_1:	spriteHeader
- spritePiece $28, $13, 2, 1, $11D, 0, 0, 0, 0
- spritePiece $38, $13, 1, 1, $11E, 0, 0, 0, 0
- spritePiece $40, $13, 2, 1, $11E, 0, 0, 0, 0
- spritePiece $50, $13, 2, 1, $11E, 0, 0, 0, 0
- spritePiece $60, $13, 1, 1, $120, 0, 0, 0, 0
- spritePiece $24, -3, 1, 1, $F8, 0, 0, 0, 0
- spritePiece $2C, -3, 1, 1, $FE, 0, 0, 0, 0
- spritePiece $32, -3, 1, 1, $F1, 0, 0, 0, 0
- spritePiece $3A, -3, 1, 1, $100, 0, 0, 0, 0
- spritePiece $4C, -3, 1, 1, $F8, 0, 0, 0, 0
- spritePiece $54, -3, 1, 1, $FE, 0, 0, 0, 0
- spritePiece $5A, -3, 1, 1, $F1, 0, 0, 0, 0
- spritePiece $62, -3, 1, 1, $100, 0, 0, 0, 0
- spritePiece $38, $B, 1, 1, $F8, 0, 0, 1, 0
- spritePiece $40, $B, 1, 1, $FE, 0, 0, 1, 0
- spritePiece $46, $B, 1, 1, $F6, 0, 0, 1, 0
- spritePiece $4E, $B, 1, 1, $100, 0, 0, 1, 0
-Map_1a6a_1_End
+.shield2:	dc.w 4
+	dc.w $E80A, 0, 0, $FFE8
+	dc.w $E80A, 9, 4, 0
+	dc.w $A, $1000, $1000, $FFE8
+	dc.w $A, $1009, $1004, 0
 
-Map_1a6a_2:	spriteHeader
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -$28, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
- spritePiece -$80, -8, 4, 4, 0, 0, 0, 0, 0
-Map_1a6a_2_End
+.shield3:	dc.w 4
+	dc.w $E80A, $812, $809, $FFE9
+	dc.w $E80A, $12, 9, 0
+	dc.w $A, $1812, $1809, $FFE9
+	dc.w $A, $1012, $1009, 0
 
-Map_1a6a_3:	spriteHeader
- spritePiece -8, -4, 3, 1, $10D, 0, 0, 1, 0
- spritePiece $10, -4, 1, 1, $10A, 0, 0, 1, 0
-Map_1a6a_3_End
+.shield4:	dc.w 4
+	dc.w $E80A, $809, $804, $FFE8
+	dc.w $E80A, $800, $800, 0
+	dc.w $A, $1809, $1804, $FFE8
+	dc.w $A, $1800, $1800, 0
+
+.stars1:	dc.w 4
+	dc.w $E80A, 0, 0, $FFE8
+	dc.w $E80A, 9, 4, 0
+	dc.w $A, $1809, $1804, $FFE8
+	dc.w $A, $1800, $1800, 0
+
+.stars2:	dc.w 4
+	dc.w $E80A, $809, $804, $FFE8
+	dc.w $E80A, $800, $800, 0
+	dc.w $A, $1000, $1000, $FFE8
+	dc.w $A, $1009, $1004, 0
+
+.stars3:	dc.w 4
+	dc.w $E80A, $12, 9, $FFE8
+	dc.w $E80A, $1B, $D, 0
+	dc.w $A, $181B, $180D, $FFE8
+	dc.w $A, $1812, $1809, 0
+
+.stars4:	dc.w 4
+	dc.w $E80A, $81B, $80D, $FFE8
+	dc.w $E80A, $812, $809, 0
+	dc.w $A, $1012, $1009, $FFE8
+	dc.w $A, $101B, $100D, 0
 
 	even
