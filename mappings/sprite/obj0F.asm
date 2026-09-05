451876ad2773c41608d467dc1e43c454555e92cd
TheBlueGamer-Github <alexjokr@outlook.com>
2026-09-05 10:12:45 -0500

    Fix title options

--- mappings/sprite/obj0F.asm
+++ mappings/sprite/obj0F.asm
@@ -1,46 +1,46 @@
-Obj0F_Map: mappingsTable
-	mappingsTableEntry.w	Obj0F_Map_0
-	mappingsTableEntry.w	Obj0F_Map_1
-	mappingsTableEntry.w	Obj0F_Map_2
-	mappingsTableEntry.w	Obj0F_Map_3
+Obj0FMap: mappingsTable
+	mappingsTableEntry.w	Obj0FMap_0
+	mappingsTableEntry.w	Obj0FMap_1
+	mappingsTableEntry.w	Obj0FMap_2
+	mappingsTableEntry.w	Obj0FMap_3
 
-Obj0F_Map_0:	spriteHeader
- spritePiece -$2C, -$C, 1, 1, $681, 0, 0, 3, 0
- spritePiece -$1C, -$C, 1, 1, $69D, 0, 0, 3, 0
- spritePiece -$14, -$C, 1, 1, $699, 0, 0, 3, 0
- spritePiece -$C, -$C, 1, 1, $68E, 0, 0, 3, 0
- spritePiece -$4, -$C, 1, 1, $6A6, 0, 0, 3, 0
- spritePiece 4, -$C, 1, 1, $692, 0, 0, 3, 0
- spritePiece $C, -$C, 1, 1, $69F, 0, 0, 3, 0
- spritePiece -$44, -$C, 2, 1, $1D5, 0, 0, 3, 0
- spritePiece -$24, 4, 1, 1, $69C, 0, 0, 3, 0
- spritePiece -$1C, 4, 1, 1, $69D, 0, 0, 3, 0
- spritePiece -$14, 4, 1, 1, $6A1, 0, 0, 3, 0
- spritePiece -$C, 4, 1, 1, $696, 0, 0, 3, 0
- spritePiece -4, 4, 1, 1, $69C, 0, 0, 3, 0
- spritePiece 4, 4, 1, 1, $69B, 0, 0, 3, 0
- spritePiece $C, 4, 1, 1, $6A0, 0, 0, 3, 0
-Obj0F_Map_0_End
+Obj0FMap_0:	spriteHeader
+ spritePiece -$2C, -$C, 1, 1, $681, 0, 0, 1, 0
+ spritePiece -$1C, -$C, 1, 1, $69D, 0, 0, 1, 0
+ spritePiece -$14, -$C, 1, 1, $699, 0, 0, 1, 0
+ spritePiece -$C, -$C, 1, 1, $68E, 0, 0, 1, 0
+ spritePiece -4, -$C, 1, 1, $6A6, 0, 0, 1, 0
+ spritePiece 4, -$C, 1, 1, $692, 0, 0, 1, 0
+ spritePiece $C, -$C, 1, 1, $69F, 0, 0, 1, 0
+ spritePiece -$44, -$C, 2, 1, $1D5, 0, 0, 0, 0
+ spritePiece -$24, 4, 1, 1, $69C, 0, 0, 1, 0
+ spritePiece -$1C, 4, 1, 1, $69D, 0, 0, 1, 0
+ spritePiece -$14, 4, 1, 1, $6A1, 0, 0, 1, 0
+ spritePiece -$C, 4, 1, 1, $696, 0, 0, 1, 0
+ spritePiece -4, 4, 1, 1, $69C, 0, 0, 1, 0
+ spritePiece 4, 4, 1, 1, $69B, 0, 0, 1, 0
+ spritePiece $C, 4, 1, 1, $6A0, 0, 0, 1, 0
+Obj0FMap_0_End
 
-Obj0F_Map_1:	spriteHeader
- spritePiece -$2C, -$C, 1, 1, $681, 0, 0, 3, 0
- spritePiece -$1C, -$C, 1, 1, $69D, 0, 0, 3, 0
- spritePiece -$14, -$C, 1, 1, $699, 0, 0, 3, 0
- spritePiece -$C, -$C, 1, 1, $68E, 0, 0, 3, 0
- spritePiece -$4, -$C, 1, 1, $6A6, 0, 0, 3, 0
- spritePiece 4, -$C, 1, 1, $692, 0, 0, 3, 0
- spritePiece $C, -$C, 1, 1, $69F, 0, 0, 3, 0
- spritePiece -$24, 4, 1, 1, $69C, 0, 0, 3, 0
- spritePiece -$1C, 4, 1, 1, $69D, 0, 0, 3, 0
- spritePiece -$14, 4, 1, 1, $6A1, 0, 0, 3, 0
- spritePiece -$C, 4, 1, 1, $696, 0, 0, 3, 0
- spritePiece -4, 4, 1, 1, $69C, 0, 0, 3, 0
- spritePiece 4, 4, 1, 1, $69B, 0, 0, 3, 0
- spritePiece $C, 4, 1, 1, $6A0, 0, 0, 3, 0
- spritePiece -$44, 4, 2, 1, $1D5, 0, 0, 3, 0
-Obj0F_Map_1_End
+Obj0FMap_1:	spriteHeader
+ spritePiece -$2C, -$C, 1, 1, $681, 0, 0, 1, 0
+ spritePiece -$1C, -$C, 1, 1, $69D, 0, 0, 1, 0
+ spritePiece -$14, -$C, 1, 1, $699, 0, 0, 1, 0
+ spritePiece -$C, -$C, 1, 1, $68E, 0, 0, 1, 0
+ spritePiece -4, -$C, 1, 1, $6A6, 0, 0, 1, 0
+ spritePiece 4, -$C, 1, 1, $692, 0, 0, 1, 0
+ spritePiece $C, -$C, 1, 1, $69F, 0, 0, 1, 0
+ spritePiece -$24, 4, 1, 1, $69C, 0, 0, 1, 0
+ spritePiece -$1C, 4, 1, 1, $69D, 0, 0, 1, 0
+ spritePiece -$14, 4, 1, 1, $6A1, 0, 0, 1, 0
+ spritePiece -$C, 4, 1, 1, $696, 0, 0, 1, 0
+ spritePiece -4, 4, 1, 1, $69C, 0, 0, 1, 0
+ spritePiece 4, 4, 1, 1, $69B, 0, 0, 1, 0
+ spritePiece $C, 4, 1, 1, $6A0, 0, 0, 1, 0
+ spritePiece -$44, 4, 2, 1, $1D5, 0, 0, 0, 0
+Obj0FMap_1_End
 
-Obj0F_Map_2:	spriteHeader
+Obj0FMap_2:	spriteHeader
  spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
  spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
  spritePiece -$80, -$48, 4, 4, 0, 0, 0, 0, 0
@@ -72,10 +72,10 @@ Obj0F_Map_2:	spriteHeader
  spritePiece -$80, -7, 4, 3, 0, 0, 0, 0, 0
  spritePiece -$80, -8, 4, 3, 0, 0, 0, 0, 0
  spritePiece -$80, -8, 4, 3, 0, 0, 0, 0, 0
-Obj0F_Map_2_End
+Obj0FMap_2_End
 
-Obj0F_Map_3:	spriteHeader
+Obj0FMap_3:	spriteHeader
  spritePiece -8, -4, 2, 1, 0, 0, 0, 0, 0
-Obj0F_Map_3_End
+Obj0FMap_3_End
 
 	even
